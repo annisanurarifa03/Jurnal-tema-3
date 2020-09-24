2fd18e460e2e33ad76443dbe672e60abf08ed743
annisanurarifa03 <nurarifaa@student.telkomuniversity.ac.id>
2020-09-24 13:05:58 +0700

    Kuadratn.java

---
+++ jurnal 3 soal/Kuadratn.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Kuadratn {
+	public static void main(String[] args) {
+	    Scanner input = new Scanner(System.in);
+	    
+		System.out.println("Masukkan angka: ");
+		int N = input.nextInt();
+	
+		System.out.println("Hasil N kuadrat: " + (N * N));
+		
+		
+	}
+}
